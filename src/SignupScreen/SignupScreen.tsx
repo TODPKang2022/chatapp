a6f2fa1ecb626a67c67a6404134c6150b1d02202
TODPKang2022 <eastsky21@gmail.com>
2024-02-06 17:28:11 +0900

    add signupscreen

--- src/SignupScreen/SignupScreen.tsx
+++ src/SignupScreen/SignupScreen.tsx
@@ -1,13 +1,63 @@
 import { useCallback, useMemo, useState } from "react";
 import Screen from "../components/Screen";
 // import validator from 'validator';
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import Colors from "../modules/Colors";
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-      },
+    },
+    section: {
+        marginBottom: 20,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: Colors.BLACK,
+    },
+    input: {
+        marginTop: 10,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        borderColor: Colors.GRAY,
+        fontSize: 16,
+    },
+    errorText: {
+        fontSize: 15,
+        color: Colors.RED,
+        marginTop: 4,
+    },
+    signupButton: {
+        backgroundColor: Colors.BLACK,
+        borderRadius: 10,
+        alignItems: 'center',
+        padding: 20,
+    },
+    signupButtonText: {
+        color: Colors.WHITE,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    disabledSignupButton: {
+        backgroundColor: Colors.GRAY,
+    },
+    signinTextButton: {
+        marginTop: 5,
+        alignItems: 'center',
+        padding: 10,
+    },
+    signinButtonText: {
+        fontSize: 16,
+        color: Colors.BLACK,
+    },
+    signingContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
 
 const SignupScreen = () => {
@@ -18,15 +68,15 @@ const SignupScreen = () => {
     const [name, setName] = useState('');
 
     // 입력값 확인
-    // const emailErrorText = useMemo(() => {
-    //     if (email.length === 0) {
-    //         return '이메일을 입력해 주세요';
-    //     }
-    //     if (!validator.isEmail(email)) {
-    //         return '올바른 이메일이 아닙니다'
-    //     }
-    //     return null;
-    // }, [email]);
+    const emailErrorText = useMemo(() => {
+        if (email.length === 0) {
+            return '이메일을 입력해 주세요';
+        }
+        // if (!validator.isEmail(email)) {
+        //     return '올바른 이메일이 아닙니다'
+        // }
+        return null;
+    }, [email]);
     
     const passwordErrorText = useMemo(() => {
         if (password.length === 0) {
@@ -77,11 +127,84 @@ const SignupScreen = () => {
         setName(text);
     }, []);
 
+
+    // return true or false 
+    const signupButtonEnabled = useMemo(() => {
+        return (
+            emailErrorText == null &&
+            passwordErrorText == null &&
+            confirmedPasswordErrorText == null &&
+            nameErrorText == null
+        );
+    },[
+        emailErrorText,
+        passwordErrorText,
+        confirmedPasswordErrorText,
+        nameErrorText
+    ]);
+
+    const signupButtonStyle = useMemo(()=> {
+        if (signupButtonEnabled){
+            return styles.signupButton;
+        }
+        return [styles.signupButton, styles.disabledSignupButton];
+    }, [signupButtonEnabled]);
     
 
     return <Screen title="회원가입">
         <ScrollView style={styles.container}>
-            
+            <View style={styles.section}>
+                <Text style={styles.title}>이메일</Text>
+                <TextInput 
+                    value={email}
+                    style={styles.input}
+                    onChangeText={onChangeEmailText}
+                    />
+                {emailErrorText && (<Text style={styles.errorText}>{emailErrorText}</Text>)} 
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>비밀번호</Text>
+                <TextInput
+                value={password}
+                style={styles.input}
+                secureTextEntry
+                onChangeText={onChangePasswordText}
+                />
+                {passwordErrorText && (
+                <Text style={styles.errorText}>{passwordErrorText}</Text>
+                )}
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>비밀번호 확인</Text>
+                <TextInput
+                value={confirmedPassword}
+                style={styles.input}
+                secureTextEntry
+                onChangeText={onChangeConfirmedPasswordText}
+                />
+                {confirmedPasswordErrorText && (
+                <Text style={styles.errorText}>{confirmedPasswordErrorText}</Text>
+                )}
+            </View>
+            <View style={styles.section}>
+                <Text style={styles.title}>이름</Text>
+                <TextInput
+                value={name}
+                style={styles.input}
+                onChangeText={onChangeNameText}
+                />
+                {nameErrorText && (
+                <Text style={styles.errorText}>{nameErrorText}</Text>
+                )}
+            </View>
+            <View>
+                <TouchableOpacity style={signupButtonStyle}>
+                    <Text style={styles.signinButtonText}>회원 가입</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.signinTextButton}>
+                    <Text>이미 계정이 있으신가요?</Text>
+                </TouchableOpacity>
+            </View>
         </ScrollView>
     </Screen>;
 }
