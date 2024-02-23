815fbfd00a45cd8de6fb38873e0a0d9a4bd0b8f7
Dongho <eastsky21@eastsky21ui-MacBookAir.local>
2024-02-23 20:14:23 +0900

    add icon

--- src/ChatScreen/ChatScreen.tsx
+++ src/ChatScreen/ChatScreen.tsx
@@ -1,13 +1,17 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
-import React from 'react';
+import React, { useState } from 'react';
+import { useMemo } from 'react';
 import { useCallback } from 'react';
 import {
   ActivityIndicator,
   FlatList,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Screen from '../components/Screen';
 import Colors from '../modules/Colors';
 import { RootStackParamList } from '../types';
@@ -46,12 +50,66 @@ const styles = StyleSheet.create({
   userProfileText: {
     color: Colors.WHITE,
   },
+  messageList: {
+    flex: 1,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInputContainer: {
+    flex: 1,
+    marginRight: 10,
+    borderRadius: 24,
+    borderColor: Colors.BLACK,
+    borderWidth: 1,
+    overflow: 'hidden',
+    padding: 10,
+    minHeight: 50,
+    justifyContent: 'center',
+  },
+  textInput: {
+    paddingTop: 0,
+    paddingBottom: 0,
+  },
+  sendButton: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.BLACK,
+    width: 50,
+    height: 50,
+    borderRadius: 50 / 2,
+  },
+  sendText: {
+    color: Colors.WHITE,
+  },
+  sendIcon: {
+    color: Colors.WHITE,
+    fontSize: 18,
+  },
 });
+
+// style을 가져와서 override
+const disabledSendButtonStyle = [
+  styles.sendButton,
+  { backgroundColor: Colors.GRAY },
+];
 
 const ChatScreen = () => {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Chat'>>();
   const { other, userIds } = params;
   const { loadingChat, chat } = useChat(userIds);
+  const [text, setText] = useState('');
+  const sendDisabled = useMemo(() => text.length === 0, [text]);
+
+  const onChangeText = useCallback((newText: string) => {
+    setText(newText);
+  }, []);
+
+  const onPressSendButton = useCallback(() => {
+    setText('');
+  }, []);
+
   console.log(chat);
   const renderChat = useCallback(() => {
     if (chat == null) {
@@ -72,9 +130,27 @@ const ChatScreen = () => {
             horizontal
           />
         </View>
+        <View style={styles.messageList} />
+        <View style={styles.inputContainer}>
+          <View style={styles.textInputContainer}>
+            <TextInput
+              style={styles.textInput}
+              value={text}
+              onChangeText={onChangeText}
+              multiline
+            />
+          </View>
+          <TouchableOpacity
+            style={sendDisabled ? disabledSendButtonStyle : styles.sendButton}
+            disabled={sendDisabled}
+            onPress={onPressSendButton}>
+            {/* <Text style={styles.sendText}>Send</Text> */}
+            <Icon style={styles.sendIcon} name="send" />
+          </TouchableOpacity>
+        </View>
       </View>
     );
-  }, [chat]);
+  }, [chat, onChangeText, text, sendDisabled, onPressSendButton]);
 
   return (
     <Screen title={other.name}>
